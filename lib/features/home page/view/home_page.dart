3abc7be0163bc70d9f12105b5aaee9eb63128adb
Sathish g <srisrisrisathish176@gmail.com>
2026-01-05 05:32:23 -0800

    updated search page

--- lib/features/home page/view/home_page.dart
+++ lib/features/home page/view/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:blood_bank/core/constants/navigation.dart';
 import 'package:blood_bank/core/widgets/custom_icon_button.dart';
 import 'package:blood_bank/core/widgets/custom_search_form_field.dart';
+import 'package:blood_bank/features/search/view/search_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,28 +16,36 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 60),
-            Row(
-              children: [
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: const AbsorbPointer(
-                      absorbing: true,
-                      child: CustomSearchFormField(),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              const SizedBox(height: 60),
+              Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        navigateTo(context: context, route: const SearchPage());
+                      },
+                      child: const Hero(
+                        tag: "search",
+                        child: AbsorbPointer(
+                          absorbing: true,
+                          child: CustomSearchFormField(),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(width: 10),
-                CustomIconButton(
-                  icon: const Icon(Icons.filter, color: Colors.white),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ],
+                  const SizedBox(width: 10),
+                  CustomIconButton(
+                    icon: const Icon(Icons.filter, color: Colors.white),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/features/search/view/search_page.dart
@@ -0,0 +1,52 @@
+import 'dart:developer';
+
+import 'package:blood_bank/core/widgets/custom_search_form_field.dart';
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final searchController = TextEditingController();
+  final FocusNode focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    focusNode.requestFocus();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              CustomSearchFormField(
+                controller: searchController,
+                focusNode: focusNode,
+                onSubmitted: (value) {
+                  log(value);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
