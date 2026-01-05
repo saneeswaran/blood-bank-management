566e0e79517027cb241000132eb6f6873182fbee
Sathish g <srisrisrisathish176@gmail.com>
2026-01-04 22:14:36 -0800

    imported assets

---
+++ lib/features/onboard/view/onboard_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class OnboardPage extends StatefulWidget {
+  const OnboardPage({super.key});
+
+  @override
+  State<OnboardPage> createState() => _OnboardPageState();
+}
+
+class _OnboardPageState extends State<OnboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+              width: double.infinity,
+              child: Image.asset(""),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
