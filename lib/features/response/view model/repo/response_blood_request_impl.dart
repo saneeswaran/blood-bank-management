1058b0e324a8477a9758108ad255a0b69b4e0dfa
Sathish g <srisrisrisathish176@gmail.com>
2026-01-21 20:01:05 +0530

    storing request into user data for flexible

--- lib/features/response/view model/repo/response_blood_request_impl.dart
+++ lib/features/response/view model/repo/response_blood_request_impl.dart
@@ -1,14 +1,23 @@
 import 'package:blood_bank/core/constants/constats.dart';
 import 'package:blood_bank/features/profile/model/model/user_model.dart';
 import 'package:blood_bank/features/response/model/model/accept_donation.dart';
+import 'package:blood_bank/features/response/view%20model/repo/response_blood_request.dart';
 import 'package:fpdart/fpdart.dart';
 
-abstract class ResponseBloodRequest {
+class ResponseBloodRequestImpl extends ResponseBloodRequest {
+  @override
   Future<Either<Failure, List<UserModel>>> fetchAccesptedDonors({
     required String donationId,
-  });
+  }) async {
+    // TODO: implement fetchAccesptedDonors
+    throw UnimplementedError();
+  }
 
+  @override
   Future<Either<Failure, List<AcceptDonation>>> fetchDonationRequests({
     required String requestId,
-  });
+  }) async {
+    // TODO: implement fetchDonationRequests
+    throw UnimplementedError();
+  }
 }
